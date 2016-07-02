c5ebd13b795ca711888092ee969506aa42f6eb8c
XiaoMinwm <xiaominomg@gmail.com>
2016-07-02 14:49:06 +0800

    ok

--- verifier_test.py
+++ verifier_test.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
 	waitSeq = [['Q0Q43Q44Q45Q46Q4Q10Q48Q63Q61Q71Q52Q53Q50Q54Q55Q56Q57Q58Q46'],['Q0Q81Q82Q84Q85Q86Q96Q130']]
 	for seq in waitSeq:
 		if verifier(seq,model[3],datainput(50)[0]):
-			visualizeAttack(seq, model, datainput(50)[0], 4)
+			visualizeAttack(seq, model, datainput(50)[0], 3)
