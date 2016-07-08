2830bc3bd8b6af3c9b67ab5bc381e459a39f45d8
XiaoMinwm <xiaomin7214@163.com>
2016-07-08 11:32:32 +0800

    ok

--- verifier_test.py
+++ verifier_test.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     for seq in waitSeq:
         if verifier(seq, model[3], datainput(50)[0]):
             visualizeAttack(seq, model, datainput(50)[0], 4)
-            print('github test xm!')
+            print('github test last!')
