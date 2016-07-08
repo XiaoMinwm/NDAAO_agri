8ceff25c8a60c49b171fe2d1119da9645cdbe6ec
xm <xiaomin7214@163.com>
2016-07-08 10:22:46 +0800

    just for test!

--- verifier_test.py
+++ verifier_test.py
@@ -3,8 +3,9 @@ from data_input import datainput
 from verifier import verifier
 
 if __name__ == '__main__':
-	model = dotdraw_net(4,50)
-	waitSeq = [['Q0Q43Q44Q45Q46Q4Q10Q48Q63Q65Q41Q51'],['Q0Q81Q82Q84Q85Q86Q96Q130']]
-	for seq in waitSeq:
-		if verifier(seq,model[3],datainput(50)[0]):
-			visualizeAttack(seq, model, datainput(50)[0], 4)
+    model = dotdraw_net(4, 50)
+    waitSeq = [['Q0Q43Q44Q45Q46Q4Q10Q48Q63Q65Q41Q51'], ['Q0Q81Q82Q84Q85Q86Q96Q130']]
+    for seq in waitSeq:
+        if verifier(seq, model[3], datainput(50)[0]):
+            visualizeAttack(seq, model, datainput(50)[0], 4)
+            print('github test!')
